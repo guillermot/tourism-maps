1fc25584f5cc90296f34cfef8f496ae92e8d50d1
Guillermo Tamanaha <guillermo.tamanaha@outlook.com>
2017-05-17 15:16:05 -0300

    aa

---
+++ assets/js/index.js
@@ -0,0 +1,68 @@
+var loadPlaces = function () {
+    const grid = document.getElementById('grid-places');
+    grid.innerHTML = '';
+
+    places.forEach(place => {
+        // Creating elements
+        const item = document.createElement('div');
+        const anchor = document.createElement('a');
+
+        const imageContainer = document.createElement('div');
+        const image = document.createElement('img');
+
+        const descriptionContainer = document.createElement('div');
+        const title = document.createElement('div');
+        const description = document.createElement('div');
+
+        // Adding row
+        grid.appendChild(item);
+
+        // Building card layout
+        item.appendChild(anchor);
+        anchor.appendChild(imageContainer);
+        anchor.appendChild(descriptionContainer);
+        imageContainer.appendChild(image);
+        descriptionContainer.appendChild(title);
+        descriptionContainer.appendChild(description);
+
+        // Applying styles
+        item.setAttribute('class', 'item');
+        anchor.setAttribute('class', 'item-clickeable');
+        imageContainer.setAttribute('class', 'image-container');
+        descriptionContainer.setAttribute('class', 'description-container');
+        title.setAttribute('class', 'title');
+        description.setAttribute('class', 'description');
+
+        // Setting values
+        item.setAttribute('data-id', place.id);
+        item.setAttribute('data-lat', place.lat);
+        item.setAttribute('data-lng', place.lng);
+        title.innerText = place.title;
+        description.innerText = place.description;
+        image.setAttribute('src', place.img);
+
+        // Adding marker
+        // var marker = new google.maps.Marker({
+        //   position: uluru,
+        //   map: map
+        // });
+
+    });
+
+    var items = document.querySelectorAll('#grid-places .item');
+    console.log(items);
+    items.forEach(item => {
+        item.addEventListener('mouseover', event => {
+            // console.log(event.fromElement);
+            console.log(event.relatedTarget);
+            var e = event.fromElement || event.relatedTarget;
+            if (e.parentNode == this || e == this) {
+                return;
+            }
+        }, false);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+    loadPlaces();
+}, false);
---
+++ assets/js/places.js
@@ -0,0 +1,52 @@
+const places = [
+    {
+        id: 1,
+        type: 'place',
+        title: 'Rockerfeller Center',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/rockefeller-center.jpg',
+        lat: 40.758891,
+        lng: -73.978693
+    },
+    {
+        id: 3,
+        type: 'area',
+        title: 'Chinatown',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/chinatown.jpg'
+    },
+    {
+        id: 4,
+        type: 'place',
+        title: 'Grand Central Terminal',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/gran-central-terminal.jpg',
+        lat: 40.752954,
+        lng: -73.977208
+    },
+    {
+        id: 5,
+        type: 'place',
+        title: 'Brooklyn Bridge',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/brookyn-bridge.jpg',
+        lat: 40.706574,
+        lng: -73.996810
+    },
+    {
+        id: 6,
+        type: 'place',
+        title: 'Statue of Liberty',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/statue-of-liberty.jpg',
+        lat: 40.689225,
+        lng: -74.044533
+    },
+    {
+        id: 7,
+        type: 'area',
+        title: 'High Line',
+        description: 'Iconic Midtown business complex',
+        img: 'assets/img/manhattan/high-line.jpg'
+    }
+];
