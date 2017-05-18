9094bb74af36069f08dc5307c5f17afcd648b963
guillermot <guillet84@gmail.com>
2017-05-17 22:29:55 -0300

    guides

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,69 @@
-var loadPlaces = function () {
-    const grid = document.getElementById('grid-places');
+const markers = [];
+
+const loadGuides = function(guides, containerId) {
+    const grid = document.getElementById(containerId);
     grid.innerHTML = '';
+
+    grid.classList.remove('fadeIn');
+    grid.classList.remove('animated');
+
+    guides.forEach(guide => {
+
+        const item = document.createElement('div');
+        const anchor = document.createElement('a');
+
+        const imageContainer = document.createElement('div');
+        const image = document.createElement('img');
+
+        const descriptionContainer = document.createElement('div');
+        const title = document.createElement('div');
+        const description = document.createElement('div');
+
+        // Adding row
+        grid.appendChild(item);
+
+        // Building card layout
+        item.appendChild(anchor);
+        anchor.appendChild(imageContainer);
+        anchor.appendChild(descriptionContainer);
+        imageContainer.appendChild(image);
+        descriptionContainer.appendChild(title);
+        descriptionContainer.appendChild(description);
+
+        // Applying styles
+        item.setAttribute('class', 'item');
+        anchor.setAttribute('class', 'item-clickeable');
+        imageContainer.setAttribute('class', 'image-container');
+        descriptionContainer.setAttribute('class', 'description-container');
+        title.setAttribute('class', 'title');
+        description.setAttribute('class', 'description');
+
+        // Setting values
+        title.innerText = guide.name;
+        description.innerText = guide.description;
+        // image.setAttribute('src', place.img);
+
+        item.addEventListener('click', event => {
+            loadPlaces(guides, guide.places, containerId);
+        }, false);
+    });
+};
+
+const loadPlaces = function(guides, places, containerId) {
+    const grid = document.getElementById(containerId);
+    grid.innerHTML = '';
+    grid.classList.remove('fadeIn');
+    grid.classList.remove('animated');
+
+    // Backbutton
+    const backButton = document.createElement('a');
+    backButton.innerText = '<< Atras';
+    backButton.addEventListener('click', event => {
+        removeAllMarkers(markers);
+        loadGuides(guides, containerId);
+    });
+    backButton.setAttribute('class', 'back-btn');
+    grid.appendChild(backButton);
 
     places.forEach(place => {
         // Creating elements
@@ -37,34 +100,35 @@ var loadPlaces = function () {
         item.setAttribute('data-id', place.id);
         item.setAttribute('data-lat', place.lat);
         item.setAttribute('data-lng', place.lng);
-        title.innerText = place.title;
+        title.innerText = place.name;
         description.innerText = place.description;
         image.setAttribute('src', place.img);
 
         // Adding marker
-        var marker = new google.maps.Marker({
-          position: {lat: place.lat, lng: place.lng},
-          map: map,
-          title: place.title
-        });
+        if (place.lat) {
+            const coordinates = { lat: place.lat, lng: place.lng };
+            item.addEventListener('click', event => {
+                map.panTo(coordinates);
+            }, false);
 
+            var marker = new google.maps.Marker({
+                position: coordinates,
+                map: map,
+                title: place.title
+            });
+
+            markers.push(marker);
+        }
     });
 
-    var items = document.querySelectorAll('#grid-places .item');
-    console.log(items);
-    items.forEach(item => {
-        item.addEventListener('mouseover', event => {
-            // console.log(event.fromElement);
-            event.stopPropagation(); 
-            console.log(event.relatedTarget);
-            var e = event.fromElement || event.relatedTarget;
-            if (e.parentNode == this || e == this) {
-                return;
-            }
-        }, false);
+}
+
+const removeAllMarkers = (markers) => {
+    markers.forEach(marker => {
+        marker.setMap(null);
     });
 }
 
 document.addEventListener('DOMContentLoaded', event => {
-    loadPlaces();
+    loadGuides(guides, 'grid-places');
 }, false);
--- assets/js/places.js
+++ assets/js/places.js
@@ -1,52 +1,189 @@
-const places = [
-    {
-        id: 1,
-        type: 'place',
-        title: 'Rockerfeller Center',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/rockefeller-center.jpg',
-        lat: 40.758891,
-        lng: -73.978693
+const guides = [{
+        name: 'First visit!',
+        description: 'All places you need to go in your first visit to New York.',
+        places: [{
+                id: 1,
+                type: 'place',
+                name: 'Rockerfeller Center',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/rockefeller-center.jpg',
+                lat: 40.758891,
+                lng: -73.978693
+            },
+            {
+                id: 3,
+                type: 'area',
+                name: 'Chinatown',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/chinatown.jpg'
+            },
+            {
+                id: 4,
+                type: 'place',
+                name: 'Grand Central Terminal',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/gran-central-terminal.jpg',
+                lat: 40.752954,
+                lng: -73.977208
+            },
+            {
+                id: 5,
+                type: 'place',
+                name: 'Brooklyn Bridge',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/brookyn-bridge.jpg',
+                lat: 40.706574,
+                lng: -73.996810
+            },
+            {
+                id: 6,
+                type: 'place',
+                name: 'Statue of Liberty',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/statue-of-liberty.jpg',
+                lat: 40.689225,
+                lng: -74.044533
+            },
+            {
+                id: 7,
+                type: 'area',
+                name: 'High Line',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg'
+            }
+        ]
     },
     {
-        id: 3,
-        type: 'area',
-        title: 'Chinatown',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/chinatown.jpg'
+        name: 'Central Park',
+        description: 'This park is full of litles treasures to discover, each one of them will blow your mind.',
+        places: [{
+                id: 8,
+                type: 'place',
+                name: 'Belvedere Castle',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.779467,
+                lng: -73.968817
+            },
+            {
+                id: 9,
+                type: 'place',
+                title: 'Strawberry Fields Memorial',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.775738,
+                lng: -73.975186
+            },
+            {
+                id: 10,
+                type: 'place',
+                name: 'Bethesda Terrace and Fountain',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.773779,
+                lng: -73.970518
+            },
+            {
+                id: 11,
+                type: 'place',
+                name: 'Gapstow Bridge',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.766911,
+                lng: -73.973668
+            },
+            {
+                id: 12,
+                type: 'place',
+                name: 'Central Park Zoo',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.768004,
+                lng: -73.971848
+            }
+        ]
     },
     {
-        id: 4,
-        type: 'place',
-        title: 'Grand Central Terminal',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/gran-central-terminal.jpg',
-        lat: 40.752954,
-        lng: -73.977208
+        name: 'Downtown',
+        description: 'One of the most important financial',
+        places: [{
+                id: 13,
+                type: 'place',
+                name: 'World Trade Center',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.711802,
+                lng: -73.013118
+            },
+            {
+                id: 14,
+                type: 'place',
+                name: 'Charging Bull',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.705552,
+                lng: -73.013446
+            },
+
+            {
+                id: 5,
+                type: 'place',
+                name: 'Brooklyn Bridge',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/brookyn-bridge.jpg',
+                lat: 40.706574,
+                lng: -73.996810
+            }
+        ]
     },
     {
-        id: 5,
-        type: 'place',
-        title: 'Brooklyn Bridge',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/brookyn-bridge.jpg',
-        lat: 40.706574,
-        lng: -73.996810
-    },
-    {
-        id: 6,
-        type: 'place',
-        title: 'Statue of Liberty',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/statue-of-liberty.jpg',
-        lat: 40.689225,
-        lng: -74.044533
-    },
-    {
-        id: 7,
-        type: 'area',
-        title: 'High Line',
-        description: 'Iconic Midtown business complex',
-        img: 'assets/img/manhattan/high-line.jpg'
+        name: 'Neighborhoods',
+        description: 'Most important neighborhoods of Manhattan. All the right places you should visit',
+        places: [{
+                id: 15,
+                type: 'place',
+                name: 'Upper West Side',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.787159,
+                lng: -73.975835
+            },
+            {
+                id: 16,
+                type: 'place',
+                name: 'Greenwich Village',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.733323,
+                lng: -73.003037
+            },
+            {
+                id: 17,
+                type: 'place',
+                name: 'Upper East Side',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.773680,
+                lng: -73.957356
+            },
+            {
+                id: 18,
+                type: 'place',
+                name: 'Midtown',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.754514,
+                lng: -73.985984
+            },
+            {
+                id: 19,
+                type: 'place',
+                name: 'Downtown',
+                description: 'Iconic Midtown business complex',
+                img: 'assets/img/manhattan/high-line.jpg',
+                lat: 40.722913,
+                lng: -74.000018
+            },
+        ]
     }
 ];
